fe10b74e418cc1d1d613afaedc43238dbfc09d21
PrAndrian <princydruh@gmail.com>
2023-10-06 23:52:51 +0200

    phonetical checking upgrade

--- src/utils/phoneticalComparaisonStrong.js
+++ src/utils/phoneticalComparaisonStrong.js
@@ -1,51 +1,55 @@
-/**
- * Calculates the Soundex code for a given string.
- *
- * @param {string} str - The input string for which to calculate the Soundex code.
- * @returns {string} The calculated Soundex code for the input string.
- */
-function calculateSoundex(str) {
-    const upperStr = str.toUpperCase();
-    const firstLetter = upperStr.charAt(0);
+function computePhoneticCode(str, language = 'english') {
+    // Step 1: Remove leading spaces and convert to uppercase
+    str = str.trim().toUpperCase();
     
-    // Replace certain letters with their corresponding Soundex digits
-    const soundexMap = {
-        B: '1', F: '1', P: '1', V: '1',
-        C: '2', G: '2', J: '2', K: '2', Q: '2', S: '2', X: '2', Z: '2',
-        D: '3', T: '3',
-        L: '4',
-        M: '5', N: '5',
-        R: '6'
-    };
-
-    // Initialize the Soundex code with the first letter of the string
-    let soundexCode = firstLetter;
-    let prevDigit = soundexMap[firstLetter];
-
-    for (let i = 1; i < upperStr.length; i++) {
-        const char = upperStr.charAt(i);
-        const soundexDigit = soundexMap[char];
-        
-        // Ignore characters that don't have a Soundex mapping
-        if (soundexDigit) {
-            // Ignore consecutive duplicate digits
-            if (soundexDigit !== prevDigit) {
-                soundexCode += soundexDigit;
-            }
-        } else if (char !== 'H' && char !== 'W') {
-            // Ignore 'H' and 'W' if they are not separating consonants with the same code
-            prevDigit = null;
-        }
-
-        // Update the previous digit
-        prevDigit = soundexDigit || prevDigit;
+    // Step 2: Keep the first letter
+    const firstLetter = str.charAt(0);
+  
+    // Step 3: Remove specified vowels and letters
+    const vowels = language === 'french' ? 'AEIOUY' : 'AEHIOUWY';
+    let code = firstLetter;
+    for (let i = 1; i < str.length; i++) {
+      const char = str.charAt(i);
+      if (!vowels.includes(char)) {
+        code += char;
+      }
     }
-
-    // Pad with zeros to make it 4 characters long
-    soundexCode = soundexCode.padEnd(4, '0');
-
-    return soundexCode;
-}
+  
+    // Step 4: Assign numeric values
+    const letterToValue = language === 'french' ? {
+      'B': 1, 'P': 1,
+      'C': 2, 'K': 2, 'Q': 2,
+      'D': 3, 'T': 3,
+      'L': 4,
+      'M': 5, 'N': 5,
+      'R': 6,
+      'G': 7, 'J': 7,
+      'X': 8, 'Z': 8, 'S': 8,
+      'F': 9, 'V': 9
+    } : {
+      'B': 1, 'F': 1, 'P': 1, 'V': 1,
+      'C': 2, 'G': 2, 'J': 2, 'K': 2, 'Q': 2, 'S': 2, 'X': 2, 'Z': 2,
+      'D': 3, 'T': 3,
+      'L': 4,
+      'M': 5, 'N': 5,
+      'R': 6
+    };
+  
+    let phoneticCode = '';
+    for (let i = 0; i < code.length; i++) {
+      const char = code.charAt(i);
+      if (i === 0 || char !== code.charAt(i - 1)) {
+        phoneticCode += letterToValue[char];
+      }
+    }
+  
+    // Step 5: Return the first 4 characters padded with zeros
+    while (phoneticCode.length < 4) {
+      phoneticCode += '0';
+    }
+  
+    return phoneticCode;
+  }
 
 /**
  * Compares two strings for phonetic similarity using the Soundex algorithm.
@@ -55,8 +59,8 @@ function calculateSoundex(str) {
  * @returns {boolean} `true` if the strings are phonetically alike according to Soundex, `false` otherwise.
  */
 export function areStringsPhoneticallyAlike(str1, str2) {
-    const soundex1 = calculateSoundex(str1);
-    const soundex2 = calculateSoundex(str2);
+    const soundex1 = computePhoneticCode(str1);
+    const soundex2 = computePhoneticCode(str2);
 
     // Compare the Soundex codes for phonetic similarity
     return soundex1 === soundex2;
